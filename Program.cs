1c64b7b53e86dc206bf91538199a52348b8ff935
Виктор Корнеплод <yarmisikov@gmail.com>
2024-10-28 20:32:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. вывести в столбик 10 раз слово "Привет";
+            //int i = 0;
+            //while (i < 10)
+            //{
+            //    Console.WriteLine("Привет");
+            //    i++;
+            //}
+            //int i = 0;
+
+            //do
+            //{
+            //    Console.WriteLine("Привет");
+            //    i++;
+            //} while (i < 10);
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Привет");
+            //}
+            //2. вывести в столбик первые 10 натуральных чисел;
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //int i = 1;
+
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //} while (i <= 10);
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //3.вывести в столбик первые 5 натуральных чисел в обратном порядке;
+            //int i = 5;
+            //while (i >= 1)
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+
+            //int i = 5;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //} while (i >= 1);
+
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //4.вывести на экран целые числа из интервала от 1 до n;
+            //Console.Write("n=");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //int i = 1;
+            //while (i <= n)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //Console.Write("n=");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //int i = 1;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //} while (i <= n);
+
+            //Console.Write("n=");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //5.запросить у пользователя 5 слов, после чего вывести вначале дважды каждое нечетное слово
+            //и затем трижды каждое четное слово, между выводимыми словами вставить разделитель в виде трех символьных пробелов(“   ”).
+            //string[] words = new string[5];
+            //Console.WriteLine("Введите 5 слов:");
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    words[i] = Console.ReadLine();
+            //}
+            //Console.WriteLine("Результат:");
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    if (i % 2 == 0) 
+            //    {
+            //        Console.Write(words[i] + "   " + words[i] + "   ");
+            //    }
+            //    else 
+            //    {
+            //        Console.Write(words[i] + "   " + words[i] + "   " + words[i] + "   ");
+            //    }
+            //}
+            //Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
